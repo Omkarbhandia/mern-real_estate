dbb7f433fd0cf74dec1ea3958cc8ffd7165a4d2d
Omkarbhandia <sharmaomkar715@gmail.com>
2023-12-06 12:44:27 +0530

    Completed UI for Listing Page.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -283,7 +283,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -299,7 +301,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">(₹ / month)</span>
+                  {formData.type === 'rent' && (
+                  <span className='text-xs'>(₹ / month)</span>
+                )}
                 </div>
               </div>
             )}
