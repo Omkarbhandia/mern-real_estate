7be9b646ab978b60d34d35cc342f9f9a53af7e59
Omkarbhandia <sharmaomkar715@gmail.com>
2023-12-10 17:36:12 +0530

    Completed About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,39 @@
 
 export default function About() {
   return (
-    <div>About Me</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-xl sm:text-3xl font-bold mb-4 text-slate-800">
+        About <span className="text-slate-500">Hive</span>{" "}
+        <span className="text-slate-700">Homes</span>{" "}
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Hive Homes, where we redefine the essence of modern living.
+        At Hive Homes, we believe that a home is more than just a physical
+        space; it&apos;s a dynamic hub where lives flourish and communities thrive.
+        Our mission is to provide a seamless and enriching real estate
+        experience, transforming the way individuals connect with their ideal
+        homes.
+      </p>
+      <p className="mb-4 text-slate-700">
+        As the name suggests, Hive Homes symbolizes a collaborative and
+        harmonious approach to living. We envision homes as interconnected
+        spaces that nurture a sense of community, innovation, and
+        sustainability. With a commitment to excellence, we curate living spaces
+        that harmonize functionality, aesthetics, and the latest in smart home
+        technology.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Hive Homes, we understand that each individual and family is unique.
+        That&apos;s why we offer a diverse range of properties, from urban studios to
+        suburban family homes, each meticulously designed to cater to different
+        lifestyles. Our team of dedicated professionals is here to guide you
+        through the entire home-buying or selling journey, ensuring a smooth and
+        gratifying experience
+      </p>
+      <p className="mb-4 text-slate-700">
+        Welcome to Hive Homes—where your home is not just a place; it&apos;s a
+        thriving part of a buzzing community.
+      </p>
+    </div>
+  );
 }
