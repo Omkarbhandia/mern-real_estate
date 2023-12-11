c847a2b7700961008597c53cae0f5a37d2b61792
Omkarbhandia <sharmaomkar715@gmail.com>
2023-12-11 19:08:18 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -1,41 +1,54 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
-import authRouter from "./routes/authRoute.js"
+import authRouter from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
-import listingRouter from './routes/listingRoute.js'
+import listingRouter from "./routes/listingRoute.js";
+import path from "path";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     console.log("Database Connected Successfully!");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port 3000 !");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 !");
+});
 
 ////routes
 
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/listing', listingRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 //middlewares
-app.use((err, req, res, next) =>{
-    const statuscode = err.statuscode || 500;
-    const message = err.message || "Internal Server Error"
-    return res.status(statuscode).json({
-        success: false,
-        message,
-        statuscode,
-    })
-}) 
+app.use((err, req, res, next) => {
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statuscode).json({
+    success: false,
+    message,
+    statuscode,
+  });
+});
